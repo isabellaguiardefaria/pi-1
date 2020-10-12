19fde1654f9b486417685627890260224ddb440b
isabellaguiardefaria <40308412+isabellaguiardefaria@users.noreply.github.com>
2020-10-12 19:18:18 -0300

    Espaçamento dos cards
    Co-Authored-By: Nathan Oliveira <20484494+nathanoliveiras@users.noreply.github.com>
    Co-Authored-By: NaimeDavid <72144962+NaimeDavid@users.noreply.github.com>

--- js/noticias.js
+++ js/noticias.js
@@ -1,15 +1,33 @@
 let containerNoticias = document.querySelector ("#listaDeNoticias");
+//let containerCarrosel = document.querySelector ("#carrosselDeNoticias");
 async function getNoticias() {
     let resposta = await fetch ("http://newsapi.org/v2/top-headlines?apiKey=f3cc6c8c530a4a9c814ab1807c454a18&country=br");
 
     let noticias = await resposta.json();
+
+    //for (let j=0; j < 3; j++) {
+    //  let noticia = noticias.articles[j];
+      //let carroselNoticia = `
+      //<div class="carousel-inner">
+        //<div class="carousel-item active">
+          //<img class="d-block w-100" src="${noticia.urlToImage}" alt="Primeiro Slide">
+          //<div class="carousel-caption d-none d-md-block">
+            //<h5>${noticia.title}</h5>
+            //<p>${noticia.description}</p>
+            //<a href="${noticia.url}">Notícia completa</a>
+          //</div>
+        //</div>
+      //</div>
+      //`;
+      //containerCarrosel.innerHTML += carroselNoticia;
+    //}
     
 
     for (let i = 0; i< noticias.articles.length; i++) {
         let noticia = noticias.articles[i];
 
         let cardNoticia = `
-        <div class="col-3">
+        <div class="col-md-3 mb-3">   
                 <div class="card" w-50>
                     <img src="${noticia.urlToImage}" 
                     class="card-img-top" alt="...">
