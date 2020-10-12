98e713a8754afea20077b535d8a20a47c4e0d1eb
isabellaguiardefaria <40308412+isabellaguiardefaria@users.noreply.github.com>
2020-10-12 16:23:20 -0300

    Update tecnologia.js
    Co-Authored-By: Nathan Oliveira <20484494+nathanoliveiras@users.noreply.github.com>

--- js/tecnologia.js
+++ js/tecnologia.js
@@ -1,6 +1,6 @@
 let containerNoticias = document.querySelector ("#listaDeNoticias");
 async function getNoticias() {
-    let resposta = await fetch ("http://newsapi.org/v2/top-headlines?apiKey=f3cc6c8c530a4a9c814ab1807c454a18&country=br&category=technology");
+    let resposta = await fetch ("http://newsapi.org/v2/top-headlines?apiKey=f3cc6c8c530a4a9c814ab1807c454a18&country=br");
 
     let noticias = await resposta.json();
     
